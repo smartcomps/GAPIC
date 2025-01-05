4061bb03688c9caa33bb418870cbd93ef0aaa32a
Binary <130375355+smartcomps@users.noreply.github.com>
2025-01-05 00:24:08 -0800

    Update app.py

--- app.py
+++ app.py
@@ -173,7 +173,7 @@ with st.container():
                             "temperature": 1,
                             "top_p": 0.95,
                             "top_k": 40,
-                            "max_output_tokens": 8192,
+                            "max_output_tokens": 10000,
                             "response_mime_type": "text/plain",
                         }
 
